c29845a181604893d47032ef841f91cca898dc49
Hossein Khani <mhkhan2195@gmail.com>
2026-06-16 11:34:13 +0330

    Build the Domain layer and create Tenant and User Entities and implement the domain test for them

---
+++ src/FinGuard.Domain/Enums/AnomalyType.cs
@@ -0,0 +1,9 @@
+﻿namespace FinGuard.Domain.Enums;
+
+public enum AnomalyType : byte
+{
+    None = 1,
+    DuplicatedInvoicing = 2,
+    VelocitySpike = 3,
+    StatisticalAnomaly =4
+}
---
+++ src/FinGuard.Domain/Enums/FileStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FinGuard.Domain.Enums;
+
+public enum FileStatus : byte
+{
+    Uploaded = 1,
+    Processing = 2,
+    Completed = 3,
+    Failed = 4
+}
